f59287cf36f886149d22004d628ce5dd916c2600
Hmz1hb <hamzalachehabe@gmail.com>
2023-03-16 19:26:47 +0100

    minor tweeks

--- admin-emprunt-log.php
+++ admin-emprunt-log.php
@@ -190,7 +190,7 @@
     <input id="search-input" class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
     <ul class="navbar-nav px-3">
       <li class="nav-item text-nowrap">
-        <a class="nav-link" href="./logout.php">Sign out</a>
+      <a class="nav-link" href="./logout.php">Sign out - <?php echo $result['bib_nom'];?></a>
       </li>
     </ul>
   </nav>
